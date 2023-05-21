9fc36531cd9506e0cdd13f47f66878eaead0ac44
Archisman Das <72722967+Arch0125@users.noreply.github.com>
2023-05-21 06:19:15 +0530

    remove: onrpc requests

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -5,42 +5,12 @@ import {ethers} from 'ethers';
 import {Simulator} from './helpers/Simulator';
 const checkForPhishing = require('eth-phishing-detect')
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
-  }
-};
-
 export const onTransaction: OnTransactionHandler = async ({
   transaction,
   chainId,
   transactionOrigin,
 }) => {
+
   const {walletin,walletout} = await Simulator(transaction,chainId);
 
   const insights = [
@@ -54,7 +24,6 @@ export const onTransaction: OnTransactionHandler = async ({
   }else{
     warning = '**✅ This site has not been reported as a phishing site.**'
   }
-
 
   return {
     content: panel([
